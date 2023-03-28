3fe0ff2daad282e431ed13c8d80e6f74bde3ceb1
Satvik G S <1gssatvik@gmail.com>
2023-03-28 12:23:22 +0530

    added some repository interfaces'

---
+++ src/main/java/com/thoughtclan/bikerentalsystem/repositories/BikeDetailsRepository.java
@@ -0,0 +1,7 @@
+package com.thoughtclan.bikerentalsystem.repositories;
+
+import com.thoughtclan.bikerentalsystem.models.BikeDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BikeDetailsRepository extends JpaRepository<BikeDetails,Integer> {
+}
---
+++ src/main/java/com/thoughtclan/bikerentalsystem/repositories/BookingRepository.java
@@ -0,0 +1,13 @@
+package com.thoughtclan.bikerentalsystem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.thoughtclan.bikerentalsystem.models.Booking;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking,Integer> {
+
+
+}
